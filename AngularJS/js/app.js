c4927287f41bf9fea804fced579c0c56cc0a34f9
sitakant1991 <sitakant1991@gmail.com>
2015-03-20 01:39:56 +0530

    Correcting the Controller Code

---
+++ AngularJS/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+app.controller('MyController', function($scope) {
+	$scope.contacts=[];
+	$scope.add=function(){
+		$scope.contacts.push($scope.contact);
+		$scope.contact="";
+	}
+});
